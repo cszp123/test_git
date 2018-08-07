df601fdc5edfa5f2534e73d43f77aea754b10339
AFAAW-704030720\Administrator <406566640@qq.com>
2018-08-07 10:42:10 +0800

    新增配送单

---
+++ order_sub.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @Author: anchen
+ * @Date:   2018-08-07 10:33:59
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2018-08-07 10:35:07
+ */
+/**
+ * 
+ */
+class OrderSub
+{
+    /**
+     * [初始化]
+     * @return [type] [description]
+     */
+    public function index() {
+        $a = 4;
+    }
+}
