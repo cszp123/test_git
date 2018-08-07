6a409fd3615201bec91da13d056392fe3a171831
AFAAW-704030720\Administrator <406566640@qq.com>
2018-08-07 16:11:01 +0800

    Revert "哇哇哇"
    This reverts commit 5412910aaa4989a73f7b10fc69f0192e06a11859.

--- order_sub.php
+++ order_sub.php
@@ -4,7 +4,7 @@
  * @Author: anchen
  * @Date:   2018-08-07 10:33:59
  * @Last Modified by:   anchen
- * @Last Modified time: 2018-08-07 14:04:38
+ * @Last Modified time: 2018-08-07 10:35:07
  */
 /**
  * 
@@ -17,15 +17,5 @@ class OrderSub
      */
     public function index() {
         $a = 4;
-    }
-
-    public function addOrder() {
-
-        $mOrder = new Order();
-        $mOrder->insert(['order_id' => '11111', 'status' => 2]);
-    }
-
-    public function dd() {
-        
     }
 }
