05b00d4cdfcca19d583e4dd41230f38fceee7b75
AFAAW-704030720\Administrator <406566640@qq.com>
2018-08-07 16:18:27 +0800

    订单2 呀

---
+++ order2.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @Author: anchen
+ * @Date:   2018-08-07 16:17:59
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2018-08-07 16:18:17
+ */
+/**
+ * 
+ */
+class order2
+{
+    
+    public function index() {
+        echo '2';
+    }
+}
